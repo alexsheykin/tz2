397adca414257dfec1e685192660971e662c46b5
alexsheykin <168637082+alexsheykin@users.noreply.github.com>
2024-05-22 15:22:24 +0300

    Add files via upload

---
+++ папка/test.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class nptest {
+    @Test
+    public void testMin() {
+        int[] numbers = {1, 4, 2, 3};
+        assertEquals(1, np._min(numbers));
+    }
+    @Test
+    public void testMax() {
+        int[] numbers = {1, 4, 2, 3};
+        assertEquals(4, np._max(numbers));
+    }
+    @Test
+    public void testSum() {
+        int[] numbers = {1, 4, 2, 3};
+        assertEquals(10, np._sum(numbers));
+    }
+    @Test
+    public void testMult() {
+        int[] numbers = {1, 4, 2, 3};
+        assertEquals(24, np._mult(numbers));
+    }
+    @Test
+    public void testraboti() {
+        int[] largeNumbers = new int[1_000_000];
+        Arrays.fill(largeNumbers, 1);
+
+        long startTime = System.nanoTime();
+        np._min(largeNumbers);
+        long endTime = System.nanoTime();
+        System.out.println("Min время исполнения " + (endTime - startTime) + " ns");
+
+        startTime = System.nanoTime();
+        np._max(largeNumbers);
+        endTime = System.nanoTime();
+        System.out.println("Max время исполнения " + (endTime - startTime) + " нс");
+        
+        startTime = System.nanoTime();
+        np._sum(largeNumbers);
+        endTime = System.nanoTime();
+        System.out.println("Sum время исполнения " + (endTime - startTime) + " нс");
+
+        startTime = System.nanoTime();
+        np._mult(largeNumbers);
+        endTime = System.nanoTime();
+        System.out.println("Mult время исполнения " + (endTime - startTime) + " нс");
+    }
+    @Test
+    public void tst() {
+        int[] numbers = {-1, -4, -2, -3};
+        assertEquals(-4, np._min(numbers));
+        assertEquals(-1, np._max(numbers));
+        assertEquals(-10, np._sum(numbers));
+        assertEquals(-24, np._mult(numbers));
+    }
+}
